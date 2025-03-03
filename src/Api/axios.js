80963845320c190bdf87ad83e38296df4269c92b
samiz1971 <samsonzewdie@yahoo.com>
2025-03-03 21:59:22 +0000

    backend deployment using cloud firebase function

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-2025-ece77/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/clone-2025-ece77/us-central1/api",
+  baseURL: "https://us-central1-clone-2025-ece77.cloudfunctions.net/api",
 });
 
 export { axiosInstance };
